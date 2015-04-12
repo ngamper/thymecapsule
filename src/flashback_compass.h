857d7de4e7f35b1d7cb13effe7e70e3048e85423
Andrew Putlock <aputlock@users.noreply.github.com>
2015-04-11 23:16:32 -0400

    Arrow updates
    - unpredictable compass updates
    - still can't display distance

--- src/flashback_compass.h
+++ src/flashback_compass.h
@@ -3,8 +3,15 @@
 
 #define ARROW_WIDTH 20
 #define ARROW_HEIGHT 30
-#define GRANULARITY 10    // update threshold in degrees
-#define KEY_DATA 18       // communication key
+#define GRANULARITY 114
+#define CUR_X 0
+#define CUR_Y 1
+#define END_X 2
+#define END_Y 3
+#define DIR 4
+#define DIST 5
+
+#define PI 3.14159265358979323846
 
 typedef struct {
   float x, y;
